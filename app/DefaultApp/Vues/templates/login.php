1384609c12bde9b15d8c071cba0c687711ecdddc
fater <wilkerdorevlus@yahoo.com>
2019-09-19 15:25:46 -0400

    mise en place du template

--- app/DefaultApp/Vues/templates/login.php
+++ app/DefaultApp/Vues/templates/login.php
@@ -1,22 +1,33 @@
-
+<?php use systeme\Application\Application as app;?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" content="los-framework">
-    <meta name="author" content="Alcindor losthelven">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="description" content="Responsive Admin Template" />
+    <meta name="author" content="RedstarHospital" />
     <title><?php if(isset($titre))echo $titre; ?></title>
-    <!-- Main Styles -->
-    <link rel="stylesheet" href="">
+   <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css" />
+    <link href="<?= app::css("font-awesome.min") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::autre("assets/iconic/css/material-design-iconic-font.min.css") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::autre("assets/bootstrap/css/bootstrap.min.css") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::css("extra_pages") ?>" rel="stylesheet" type="text/css"/>
+
 </head>
 
 <body>
-<div id="contenue">
-    <?php
-    if(isset($contenue)){echo $contenue;}else{echo "pas de contenue";}
-    ?>
+<div class="limiter">
+    <div class="container-login100 page-background">
+        <div class="wrap-login100">
+            <?php
+            if(isset($contenue)){echo $contenue;}else{echo "pas de contenue";}
+            ?>
+        </div>
+    </div>
 </div>
+<script src="<?php echo app::autre("assets/jquery.min.js") ?>"></script>
+<script src="<?php echo app::autre("assets/bootstrap/js/bootstrap.min.js") ?>"></script>
+<script src="<?php echo app::autre("assets/login.js") ?>"></script>
 </body>
 </html>
--- app/DefaultApp/Vues/templates/principale.php
+++ app/DefaultApp/Vues/templates/principale.php
@@ -1,21 +1,245 @@
+<?php use systeme\Application\Application as app;?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" content="los-framework">
-    <meta name="author" content="Alcindor losthelven">
-    <title><?php if(isset($titre))echo $titre; ?></title>
-    <!-- Main Styles -->
-    <link rel="stylesheet" href="">
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+
+    <title><?php if (isset($titre)) echo $titre; ?></title>
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?= app::autre("assets/simple-line-icons/simple-line-icons.min.css") ?>" rel="stylesheet"
+          type="text/css"/>
+    <link href="<?= app::css("font-awesome.min") ?>" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="<?= app::autre("assets/bootstrap/css/bootstrap.min.css") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::autre("assets/material/material.min.css") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::css("material_style") ?>" rel="stylesheet" type="text/css"/>
+
+    <link href="<?= app::css("theme_style") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::css("plugins.min") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::css("style") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::css("responsive") ?>" rel="stylesheet" type="text/css"/>
+    <link href="<?= app::css("theme-color") ?>" rel="stylesheet" type="text/css"/>
+
+
 </head>
 
-<body>
-<div id="contenue">
+<body class="page-header-fixed sidemenu-closed-hidelogo page-content-white page-md header-white white-sidebar-color logo-indigo">
+<div class="page-wrapper">
+    <!-- start header -->
+    <div class="page-header navbar navbar-fixed-top">
+        <div class="page-header-inner ">
+            <!-- logo start -->
+            <div class="page-logo">
+                <a href="index.html">
+                    <span class="logo-icon fa fa-stethoscope fa-rotate-45"></span>
+                    <span class="logo-default">REDSTAR</span> </a>
+            </div>
+            <!-- logo end -->
+            <ul class="nav navbar-nav navbar-left in">
+                <li><a href="#" class="menu-toggler sidebar-toggler"><i class="icon-menu"></i></a></li>
+            </ul>
+
+            <!-- start mobile menu -->
+            <a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+                <span></span>
+            </a>
+            <!-- end mobile menu -->
+            <!-- start header menu -->
+            <div class="top-menu">
+                <ul class="nav navbar-nav pull-right">
+
+                    <!-- start notification dropdown -->
+                    <li class="dropdown dropdown-extended dropdown-notification" id="header_notification_bar">
+                        <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                            <i class="fa fa-bell-o"></i>
+                            <span class="badge headerBadgeColor1"> 6 </span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="external">
+                                <h3><span class="bold">Notifications</span></h3>
+                                <span class="notification-label purple-bgcolor">New 6</span>
+                            </li>
+                            <li>
+                                <ul class="dropdown-menu-list small-slimscroll-style" data-handle-color="#637283">
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="time">just now</span>
+                                            <span class="details">
+													<span class="notification-icon circle deepPink-bgcolor"><i class="fa fa-check"></i></span>
+													Congratulations!. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="time">3 mins</span>
+                                            <span class="details">
+													<span class="notification-icon circle purple-bgcolor"><i class="fa fa-user o"></i></span>
+													<b>John Micle </b>is now following you. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="time">7 mins</span>
+                                            <span class="details">
+													<span class="notification-icon circle blue-bgcolor"><i class="fa fa-comments-o"></i></span>
+													<b>Sneha Jogi </b>sent you a message. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="time">12 mins</span>
+                                            <span class="details">
+													<span class="notification-icon circle pink"><i class="fa fa-heart"></i></span>
+													<b>Ravi Patel </b>like your photo. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="time">15 mins</span>
+                                            <span class="details">
+													<span class="notification-icon circle yellow"><i class="fa fa-warning"></i></span> Warning! </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="time">10 hrs</span>
+                                            <span class="details">
+													<span class="notification-icon circle red"><i class="fa fa-times"></i></span> Application error. </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <div class="dropdown-menu-footer">
+                                    <a href="javascript:void(0)"> All notifications </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- end notification dropdown -->
+
+                    <!-- start manage user dropdown -->
+                    <li class="dropdown dropdown-user">
+                        <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                            <img alt="" class="img-circle " src="<?= app::autre('img/dp.jpg')?>" />
+                            <span class="username username-hide-on-mobile"> Kiran </span>
+                            <i class="fa fa-angle-down"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-default">
+                            <li>
+                                <a href="user_profile.html">
+                                    <i class="icon-user"></i> Profile </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="icon-settings"></i> Settings
+                                </a>
+                            </li>
+
+                            <li class="divider"> </li>
+
+                            <li>
+                                <a href="login.html">
+                                    <i class="icon-logout"></i> Log Out </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- end manage user dropdown -->
+
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- end header -->
+
+
+    <!-- start page container -->
+    <div class="page-container">
+        <!-- start sidebar menu -->
+        <div class="sidebar-container">
+            <div class="sidemenu-container navbar-collapse collapse fixed-menu">
+                <div id="remove-scroll" class="left-sidemenu">
+                    <ul class="sidemenu  page-header-fixed slimscroll-style" data-keep-expanded="false" data-auto-scroll="true"
+                        data-slide-speed="200" style="padding-top: 20px">
+                        <li class="sidebar-toggler-wrapper hide">
+                            <div class="sidebar-toggler">
+                                <span></span>
+                            </div>
+                        </li>
+                        <li class="nav-item <?= isset($active) ? $active : " " ?> ">
+                            <a href="#" class="nav-link ">
+                                <i class="material-icons">dashboard</i>
+                                <span class="title">Dashboard</span>
+                                <span class="selected"></span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- end sidebar menu -->
+
+        <div class="page-content-wrapper">
+            <div class="page-content">
+                <div class="page-bar">
+                    <div class="page-title-breadcrumb">
+                        <div class=" pull-left">
+                            <div class="page-title"><?= isset($entete) ? $entete : " " ?></div>
+                        </div>
+                        <ol class="breadcrumb page-breadcrumb pull-right">
+                            <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="#">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
+                            </li>
+                            <li class="active"><?= isset($entete) ? $entete : " " ?></li>
+                        </ol>
+                    </div>
+                </div>
     <?php
-    if(isset($contenue)){echo $contenue;}else{echo "pas de contenue";}
+    if (isset($contenue)) {
+        echo $contenue;
+    } else {
+        echo "pas de contenue";
+    }
     ?>
+            </div>
+        </div>
+    </div>
+    <div class="page-footer">
+        <div class="page-footer-inner"> 2019 &copy; Licence-bios 2.0
+            <a href="mailto:redstartheme@gmail.com" target="_top" class="makerCss">BIOS</a>
+        </div>
+        <div class="scroll-to-top">
+            <i class="icon-arrow-up"></i>
+        </div>
+    </div>
 </div>
+
+<script src="<?php echo app::autre("assets/jquery.min.js") ?>"></script>
+<script src="<?php echo app::autre("assets/popper/popper.js") ?>"></script>
+<script src="<?php echo app::autre("assets/jquery.blockui.min.js") ?>"></script>
+<script src="<?php echo app::autre("assets/jquery.slimscroll.js") ?>"></script>
+
+
+<script src="<?php echo app::autre("assets/bootstrap/js/bootstrap.min.js") ?>"></script>
+<script src="<?php echo app::autre("assets/bootstrap-switch/js/bootstrap-switch.min.js") ?>"></script>
+
+<script src="<?php echo app::autre("assets/counterup/jquery.waypoints.min.js") ?>"></script>
+<script src="<?php echo app::autre("assets/counterup/jquery.counterup.min.js") ?>"></script>
+
+<script src="<?php echo app::autre("assets/app.js") ?>"></script>
+<script src="<?php echo app::autre("assets/layout.js") ?>"></script>
+<script src="<?php echo app::autre("assets/theme-color.js") ?>"></script>
+
+<script src="<?php echo app::autre("assets/material/material.min.j") ?>"></script>
+
+<script src="<?php echo app::autre("assets/chart-js/Chart.bundle.js") ?>"></script>
+<script src="<?php echo app::autre("assets/chart-js/utils.js") ?>"></script>
+<script src="<?php echo app::autre("assets/chart-js/home-data.js") ?>"></script>
+
+<script src="<?php echo app::autre("") ?>"></script>
+
+
 </body>
 </html>
